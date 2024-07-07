0ddea630b8afa8c30d63d6eb7c2da9d0f357c67b
Biokes <106444765+Biokes@users.noreply.github.com>
2024-07-07 20:41:20 +0100

    add serializers

--- talent_management_app/views.py
+++ talent_management_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+
 
 # Create your views here.
+# def promote_employee(request):
+# def onboard_employee(request):
+# def employee_details(request):
+# def schedule_training(request):
+# def set_goals_for_employee(request):
+class RegisterTalent(APIView):
+    @staticmethod
+    def post(request):
+        serializer= RegisterTalentSerializer
