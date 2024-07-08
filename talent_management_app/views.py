95e83022637320061b478f4ba852aad9259db1e2
Biokes <106444765+Biokes@users.noreply.github.com>
2024-07-08 16:41:23 +0100

    update abbey to resolve conflict

--- talent_management_app/views.py
+++ talent_management_app/views.py
@@ -9,12 +9,6 @@ from rest_framework.views import APIView
 from talent_management_app.models import Talent, User, Skill
 from talent_management_app.serializers import RegisterTalentSerializer, ViewTalentProfileSerializer
 
-
-# Create your views here.
-# def promote_employee(request):
-# def employee_details(request):
-# def schedule_training(request):
-# def set_goals_for_employee(request):
 class RegisterTalent(APIView):
     @staticmethod
     @transaction.atomic
@@ -26,8 +20,7 @@ class RegisterTalent(APIView):
         email = serializer.data['email']
         password = serializer.data['password']
         phone_number = serializer.data['phone_number']
-        user = User.objects.create(email=email, password=password,
-                                   phone_number=phone_number, role='TALENT')
+        user = User.objects.create(email=email, password=password,phone_number=phone_number, role='TALENT')
         talent = Talent.objects.create(user=user)
         Skill.objects.create(skill_name=serializer.data['skil_name'], proficiency=serializer.data['proficiency'],
                              talent_id=talent.pk)
@@ -39,9 +32,11 @@ class RegisterTalent(APIView):
         if not serializer.is_valid:
             return Response({'message': 'INVALID DETAILS PROVIDED', 'success': False},
                             status=HTTP_400_BAD_REQUEST)
+
         talent = get_object_or_404(pk=serializer.data['email'])
         phone_number = talent.phone_number
         email= talent.email
         skill = Skill.objects.filter(pk=User.objects.filter(email)).get().proficiency
+
         return Response({"skill_level":f"{skill}","talent":f"{phone_number}",})
 
