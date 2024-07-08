66b7ce61772e47da859548bd5ececf87c1db20f3
Biokes <106444765+Biokes@users.noreply.github.com>
2024-07-08 12:16:07 +0100

    update

--- talent_management_app/serializers.py
+++ talent_management_app/serializers.py
@@ -6,4 +6,4 @@ from talent_management_app.models import Talent
 class RegisterTalentSerializer(serializers.Serializer):
     class Meta:
         model = Talent
-        fields = ['email','password','phone_number','skill_git name','proficiency']
+        fields = ['email','password','phone_number','skill_name','proficiency']
